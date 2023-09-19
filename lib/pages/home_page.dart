4f864c345bfc2b9e20cdb2911d6276d8f563abe7
Sam Wylie <17wyliesam@rhsstudents.co.uk>
2023-09-19 13:25:30 +0100

    19/09/23

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,13 +10,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("GCSEPotential Flashcards"),
         centerTitle: true,
         elevation: 0,
       ),
-      body: CustomScrollView(slivers: []),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: size.height * 0.40,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Padding(
+                padding: EdgeInsets.all(size.width * 0.10),
+                child: Text('home page image'),
+              ),
+            ),
+          )
+          SliverGrid(delegate: SliverChildBuilderDelegate(
+            (context, index) => Container(color:Colors.red,),
+          ), gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+          )
+          )
+        ],
+      ),
     );
   }
 }
