203a5c28caa7c9de70ed152bc90f5c2a047087b0
Sam Wylie <17wyliesam@rhsstudents.co.uk>
2023-11-14 13:24:21 +0000

    made it work to view mock up

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:secondly/configs/themes.dart';
-import 'package:secondly/notifiers/flashcards_notifier.dart';
 import 'package:secondly/pages/home_page.dart';
 
 void main() {
-  runApp(MultiProvider(child: const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
