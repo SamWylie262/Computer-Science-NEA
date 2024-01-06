e7046a7984026b8b5a2050aadc8e417e64567ca8
Sam Wylie <17wyliesam@rhsstudents.co.uk>
2024-01-06 23:56:41 +0000

    answer and bottom bar revealed on click

--- lib/pages/daily_review_page.dart
+++ lib/pages/daily_review_page.dart
@@ -12,6 +12,7 @@ class DailyReviewPage extends StatefulWidget {
 }
 
 class _DailyReviewPageState extends State<DailyReviewPage> {
+  bool showAnswerAndBottomBar = false;
   @override
   void initState() {
     super.initState();
@@ -56,114 +57,143 @@ class _DailyReviewPageState extends State<DailyReviewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Daily Review Page'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const FlashcardsPage()),
-            );
-          },
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          showAnswerAndBottomBar = true;
+        });
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Daily Review Page'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const FlashcardsPage()),
+              );
+            },
+          ),
         ),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Theme.of(context).primaryColor,
-        child: Padding(
-          padding: const EdgeInsets.all(1.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  width: 90.0,
-                  height: 70.0,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: klogo,
-                    ),
-                    child: const Column(
-                      children: [
-                        Text('Easy', style: TextStyle(color: Colors.black)),
-                        Text('month', style: TextStyle(color: Colors.black)),
-                      ],
-                    ),
-                  ),
-                ),
+              const Text(
+                'Question',
+                style: TextStyle(fontSize: 30),
               ),
-              const SizedBox(width: 0),
-              Padding(
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  width: 90.0,
-                  height: 70.0,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: klogo,
-                    ),
-                    child: const Column(
-                      children: [
-                        Text('Good', style: TextStyle(color: Colors.black)),
-                        Text('7 days', style: TextStyle(color: Colors.black)),
-                      ],
-                    ),
-                  ),
+              const SizedBox(height: 20),
+              if (showAnswerAndBottomBar)
+                const Text(
+                  'Answer',
+                  style: TextStyle(fontSize: 30),
                 ),
-              ),
-              const SizedBox(width: 0),
-              Padding(
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  width: 90.0,
-                  height: 70.0,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: klogo,
-                    ),
-                    child: const Column(
-                      children: [
-                        Text('Tricky', style: TextStyle(color: Colors.black)),
-                        Text('2 days', style: TextStyle(color: Colors.black)),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(width: 0),
-              Padding(
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  width: 87.0,
-                  height: 70.0,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: klogo,
-                    ),
-                    child: const Column(
-                      children: [
-                        Text('Hard', style: TextStyle(color: Colors.black)),
-                        Text('Today', style: TextStyle(color: Colors.black)),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
             ],
           ),
         ),
+        bottomNavigationBar: showAnswerAndBottomBar
+            ? BottomAppBar(
+                color: Theme.of(context).primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(0),
+                        child: SizedBox(
+                          width: 90.0,
+                          height: 70.0,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: klogo,
+                            ),
+                            child: const Column(
+                              children: [
+                                Text('Easy',
+                                    style: TextStyle(color: Colors.black)),
+                                Text('month',
+                                    style: TextStyle(color: Colors.black)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 0),
+                      Padding(
+                        padding: const EdgeInsets.all(0),
+                        child: SizedBox(
+                          width: 90.0,
+                          height: 70.0,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: klogo,
+                            ),
+                            child: const Column(
+                              children: [
+                                Text('Good',
+                                    style: TextStyle(color: Colors.black)),
+                                Text('7 days',
+                                    style: TextStyle(color: Colors.black)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 0),
+                      Padding(
+                        padding: const EdgeInsets.all(0),
+                        child: SizedBox(
+                          width: 90.0,
+                          height: 70.0,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: klogo,
+                            ),
+                            child: const Column(
+                              children: [
+                                Text('Tricky',
+                                    style: TextStyle(color: Colors.black)),
+                                Text('2 days',
+                                    style: TextStyle(color: Colors.black)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 0),
+                      Padding(
+                        padding: const EdgeInsets.all(0),
+                        child: SizedBox(
+                          width: 87.0,
+                          height: 70.0,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: klogo,
+                            ),
+                            child: const Column(
+                              children: [
+                                Text('Hard',
+                                    style: TextStyle(color: Colors.black)),
+                                Text('Today',
+                                    style: TextStyle(color: Colors.black)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            : null,
       ),
     );
   }
