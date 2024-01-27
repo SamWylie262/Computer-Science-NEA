217c0a335e57c0842d8dacc40d1cf7e2a4130b01
Sam Wylie <17wyliesam@rhsstudents.co.uk>
2024-01-27 22:49:36 +0000

    prototype section of code

--- lib/pages/browse_page.dart
+++ lib/pages/browse_page.dart
@@ -12,7 +12,7 @@ class BrowsePage extends StatefulWidget {
 
 class _BrowsePageState extends State<BrowsePage> {
   String searchQuery = '';
-  String dropdownValue = 'keep same';
+  String dropdownValue = 'Computing';
   final controller1 = TextEditingController();
   final controller2 = TextEditingController();
   @override
@@ -92,6 +92,7 @@ class _BrowsePageState extends State<BrowsePage> {
                               TextEditingController(text: filteredQuestion);
                           TextEditingController controller2 =
                               TextEditingController(text: filteredAnswer);
+                          dropdownValue = filteredDeck;
                           showModalBottomSheet(
                             context: context,
                             builder: (BuildContext context) {
@@ -99,131 +100,154 @@ class _BrowsePageState extends State<BrowsePage> {
                                 color: kbutton,
                                 child: Padding(
                                   padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                      ButtonTheme(
-                                        alignedDropdown: true,
-                                        child: DropdownButton<String>(
-                                          value: dropdownValue,
-                                          onChanged: (String? newValue) {
-                                            setState(() {
-                                              dropdownValue = newValue!;
-                                            });
-                                          },
-                                          items: <String>[
-                                            'keep same',
-                                            'Computing',
-                                            'English',
-                                            'Geography',
-                                            'History',
-                                            'Maths',
-                                            'Science'
-                                          ].map<DropdownMenuItem<String>>(
-                                              (String value) {
-                                            return DropdownMenuItem<String>(
-                                              value: value,
-                                              child: SizedBox(
-                                                width: 200,
-                                                child: Text(value),
+                                  child: DefaultTextStyle(
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                    child: SingleChildScrollView(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        children: <Widget>[
+                                          Row(
+                                            children: <Widget>[
+                                              const Text('Deck:'),
+                                              const Spacer(),
+                                              ButtonTheme(
+                                                alignedDropdown: true,
+                                                child: DropdownButton<String>(
+                                                  value: dropdownValue,
+                                                  onChanged:
+                                                      (String? newValue) {
+                                                    setState(() {
+                                                      dropdownValue = newValue!;
+                                                    });
+                                                  },
+                                                  items: <String>[
+                                                    'Computing',
+                                                    'English',
+                                                    'Geography',
+                                                    'History',
+                                                    'Maths',
+                                                    'Science'
+                                                  ].map<
+                                                          DropdownMenuItem<
+                                                              String>>(
+                                                      (String value) {
+                                                    return DropdownMenuItem<
+                                                        String>(
+                                                      value: value,
+                                                      child: SizedBox(
+                                                        width: 200,
+                                                        child: Text(value),
+                                                      ),
+                                                    );
+                                                  }).toList(),
+                                                ),
                                               ),
-                                            );
-                                          }).toList(),
-                                        ),
+                                            ],
+                                          ),
+                                          TextField(
+                                            controller: controller1,
+                                            decoration: const InputDecoration(
+                                              labelText:
+                                                  'Enter updated question field',
+                                            ),
+                                          ),
+                                          TextField(
+                                            controller: controller2,
+                                            decoration: const InputDecoration(
+                                              labelText:
+                                                  'Enter updated answer field',
+                                            ),
+                                          ),
+                                          ElevatedButton(
+                                            child: const Text('Apply'),
+                                            onPressed: () {
+                                              dynamic tempQuestion =
+                                                  filteredQuestion;
+                                              dynamic tempAnswer =
+                                                  filteredAnswer;
+                                              dynamic tempDeck = filteredDeck;
+                                              filteredDeck = dropdownValue;
+                                              if (tempDeck == 'Computing') {
+                                                tempDeck = 1;
+                                              }
+
+                                              if (tempDeck == 'English') {
+                                                tempDeck = 2;
+                                              }
+
+                                              if (tempDeck == 'Geography') {
+                                                tempDeck = 3;
+                                              }
+
+                                              if (tempDeck == 'History') {
+                                                tempDeck = 4;
+                                              }
+
+                                              if (tempDeck == 'Maths') {
+                                                tempDeck = 5;
+                                              }
+
+                                              if (tempDeck == 'Science') {
+                                                tempDeck = 6;
+                                              }
+                                              // Handle the apply button press
+                                              if (controller1.text.isNotEmpty) {
+                                                filteredQuestion =
+                                                    controller1.text;
+                                              }
+                                              if (controller2.text.isNotEmpty) {
+                                                filteredAnswer =
+                                                    controller2.text;
+                                              }
+                                              if (dropdownValue !=
+                                                  'keep same') {
+                                                if (filteredDeck ==
+                                                    'Computing') {
+                                                  filteredDeck = 1;
+                                                }
+
+                                                if (filteredDeck == 'English') {
+                                                  filteredDeck = 2;
+                                                }
+
+                                                if (filteredDeck ==
+                                                    'Geography') {
+                                                  filteredDeck = 3;
+                                                }
+
+                                                if (filteredDeck == 'History') {
+                                                  filteredDeck = 4;
+                                                }
+
+                                                if (filteredDeck == 'Maths') {
+                                                  filteredDeck = 5;
+                                                }
+
+                                                if (filteredDeck == 'Science') {
+                                                  filteredDeck = 6;
+                                                }
+                                              } else {
+                                                filteredDeck = tempDeck;
+                                              }
+                                              neonClient.query(
+                                                  query:
+                                                      "UPDATE cards SET question = '$filteredQuestion', answer = '$filteredAnswer', deck_id = $filteredDeck WHERE question = '$tempQuestion' AND answer = '$tempAnswer' AND deck_id = $tempDeck");
+                                              controller1.clear();
+                                              controller2.clear();
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        BrowsePage(
+                                                            key: UniqueKey())),
+                                              );
+                                            },
+                                          ),
+                                        ],
                                       ),
-                                      TextField(
-                                        controller: controller1,
-                                        decoration: const InputDecoration(
-                                          labelText:
-                                              'Enter new value for question or leave blank to keep the same',
-                                        ),
-                                      ),
-                                      TextField(
-                                        controller: controller2,
-                                        decoration: const InputDecoration(
-                                          labelText:
-                                              'Enter new value for answer or leave blank to keep the same',
-                                        ),
-                                      ),
-                                      ElevatedButton(
-                                        child: const Text('Apply'),
-                                        onPressed: () {
-                                          dynamic tempQuestion =
-                                              filteredQuestion;
-                                          dynamic tempAnswer = filteredAnswer;
-                                          dynamic tempDeck = filteredDeck;
-                                          filteredDeck = dropdownValue;
-                                          if (tempDeck == 'Computing') {
-                                            tempDeck = 1;
-                                          }
-
-                                          if (tempDeck == 'English') {
-                                            tempDeck = 2;
-                                          }
-
-                                          if (tempDeck == 'Geography') {
-                                            tempDeck = 3;
-                                          }
-
-                                          if (tempDeck == 'History') {
-                                            tempDeck = 4;
-                                          }
-
-                                          if (tempDeck == 'Maths') {
-                                            tempDeck = 5;
-                                          }
-
-                                          if (tempDeck == 'Science') {
-                                            tempDeck = 6;
-                                          }
-                                          // Handle the apply button press
-                                          if (controller1.text.isNotEmpty) {
-                                            filteredQuestion = controller1.text;
-                                          }
-                                          if (controller2.text.isNotEmpty) {
-                                            filteredAnswer = controller2.text;
-                                          }
-                                          if (dropdownValue != 'keep same') {
-                                            if (filteredDeck == 'Computing') {
-                                              filteredDeck = 1;
-                                            }
-
-                                            if (filteredDeck == 'English') {
-                                              filteredDeck = 2;
-                                            }
-
-                                            if (filteredDeck == 'Geography') {
-                                              filteredDeck = 3;
-                                            }
-
-                                            if (filteredDeck == 'History') {
-                                              filteredDeck = 4;
-                                            }
-
-                                            if (filteredDeck == 'Maths') {
-                                              filteredDeck = 5;
-                                            }
-
-                                            if (filteredDeck == 'Science') {
-                                              filteredDeck = 6;
-                                            }
-                                          } else {
-                                            filteredDeck = tempDeck;
-                                          }
-                                          neonClient.query(
-                                              query:
-                                                  "UPDATE cards SET question = '$filteredQuestion', answer = '$filteredAnswer', deck_id = $filteredDeck WHERE question = '$tempQuestion' AND answer = '$tempAnswer' AND deck_id = $tempDeck");
-                                          controller1.clear();
-                                          controller2.clear();
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    BrowsePage(
-                                                        key: UniqueKey())),
-                                          );
-                                        },
-                                      ),
-                                    ],
+                                    ),
                                   ),
                                 ),
                               );
